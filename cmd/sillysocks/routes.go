3392775bd7197155657b1c7d3db2004649f2d9ff
Jalenarms1 <Jalenarms@outlook.com>
2025-02-15 13:57:53 -0800

    updating

--- cmd/sillysocks/routes.go
+++ cmd/sillysocks/routes.go
@@ -11,6 +11,7 @@ func registerRoutes(mux *http.ServeMux) http.Handler {
 	mux.HandleFunc("/products/{id}", handlers.HandleGetProduct)
 
 	mux.HandleFunc("/checkout", handlers.HandleCreateCheckoutSession)
+	mux.HandleFunc("/checkout-wh", handlers.HandleCheckoutSessionWebhook)
 
 	wrappedMux := handlers.UseCors(mux)
 
