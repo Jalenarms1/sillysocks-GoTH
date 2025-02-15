11877431454263f372024574cd65f12333b906ff
Jalenarms1 <Jalenarms@outlook.com>
2025-02-15 14:32:40 -0800

    updating

--- internal/handlers/stripe.go
+++ internal/handlers/stripe.go
@@ -161,6 +161,7 @@ func HandleCheckoutSessionWebhook(w http.ResponseWriter, r *http.Request) {
 		status := event.Data.Object["payment_status"]
 		fmt.Println(status)
 		fmt.Println(stripe.CheckoutSessionPaymentStatusPaid)
+		fmt.Println(status == stripe.CheckoutSessionPaymentStatusPaid)
 		if status == stripe.CheckoutSessionPaymentStatusPaid {
 			orderId := event.Data.Object["metadata"].(map[string]interface{})["orderId"].(string)
 			paymentIntent := event.Data.Object["payment_intent"].(*string)
